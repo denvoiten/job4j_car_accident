28b6c1de1e90efaac7721a72a56711228df5e7fa
denvoiten <den.voiten@gmail.com>
2022-06-30 14:20:10 +0300

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/accident")
     public String index(Model model) {
+        model.addAttribute("stringList", List.of("First", "Second", "Third", "Fourth", "Fifth"));
         return "index";
+    }
+
+    @GetMapping("/")
+    public String empty() {
+        return "redirect:/accident";
     }
 }
